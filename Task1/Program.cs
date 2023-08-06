ea6e77f1f512977bd773075b7b74afa5274dd8cf
MorozovaMilena <84088568+MorozovaMilena@users.noreply.github.com>
2023-08-06 12:53:29 +0300

    Create Program.cs

---
+++ Task1/Program.cs
@@ -0,0 +1,4 @@
+Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string result = Convert.ToString(a);
+Console.WriteLine("вторая цифра числа = "+result[1]);
